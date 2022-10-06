2d29391d030fd095bd239244f3f9875cab210ec9
Tanat <tanat8511@gmail.com>
2022-10-06 17:25:34 +0600

    commit page quiz

--- src/Page/Participant/QuizPage.tsx
+++ src/Page/Participant/QuizPage.tsx
@@ -1,7 +1,31 @@
 const QuizPage = () => {
+
+
     return(
         <div className={'quiz_page'}>
+            <div className="quiz_content">
+                <div className="tab_wrapper">
+                    <button className="tab_btn first">1</button>
+                    <button className="tab_btn">2</button>
+                    <button className="tab_btn">3</button>
+                    <button className="tab_btn">4</button>
+                    <button className="tab_btn">5</button>
+                    <button className="tab_btn">6</button>
+                    <button className="tab_btn">7</button>
+                    <button className="tab_btn">8</button>
+                    <button className="tab_btn">9</button>
+                    <button className="tab_btn">10</button>
+                    <button className="tab_btn">11</button>
+                    <button className="tab_btn">12</button>
+                    <button className="tab_btn">13</button>
+                    <button className="tab_btn">14</button>
+                    <button className="tab_btn last">15</button>
+                </div>
+                <div className="quiz-information">
+                    <div className="title"><h4>1. What is HTML?</h4></div>
 
+                </div>
+            </div>
         </div>
     )
 }
